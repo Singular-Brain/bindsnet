230694bfe746406daa16922fdd9d25220a84651d
Erfan Mirzaei <erfunmirzaei@gmail.com>
2021-08-05 12:54:41 +0430

    debug manual_seed

--- manual_seed.py
+++ manual_seed.py
@@ -4,14 +4,7 @@ import torch
 
 def manual_seed(seed):
     random.seed(seed)
-    gpu = True
-    if gpu and torch.cuda.is_available():
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-    else:
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        device = "cpu"
-        if gpu:
-            gpu = False
