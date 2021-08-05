665339c935631f0bf73e906caa6767f499b5715c
Erfan Mirzaei <erfunmirzaei@gmail.com>
2021-08-05 11:39:45 +0430

    add scalar reward

--- bindsnet/learning/reward.py
+++ bindsnet/learning/reward.py
@@ -115,6 +115,7 @@ class DynamicDopamineInjection(AbstractReward):
         self.dopamine_for_correct_pred = kwargs.get('dopamine_for_correct_pred', 1.0)
         self.tc_reward = kwargs.get('tc_reward')
         self.dopamine_base = kwargs.get('dopamine_base', 0.002)
+        self.give_reward = kwargs.get('give_reward', False)
         dt = torch.as_tensor(self.dt)
         self.decay = torch.exp(-dt / self.tc_reward)
 
@@ -123,6 +124,9 @@ class DynamicDopamineInjection(AbstractReward):
         self.label = kwargs['labels']
 
         self.dopamine = self.dopamine_base
+        if self.give_reward: 
+            self.dopamine += self.dopamine_for_correct_pred
+        
         return self.dopamine
         
     def update(self, **kwargs) -> None:
