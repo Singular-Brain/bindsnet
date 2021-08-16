e8725c00d14cc30b44ad200d244b53181ea462e4
hafezgh <ghaemi.hafez@gmail.com>
2021-08-16 20:33:26 +0200

    minor changes

--- bindsnet/learning/reward.py
+++ bindsnet/learning/reward.py
@@ -171,7 +171,7 @@ class DynamicDopamineInjection(AbstractReward):
                 label_spikes = [0]*self.n_labels
                 for i in range(self.n_labels):
                     label_spikes[i] = (s[:,i*self.n_per_class:(self.label+1)*self.n_per_class,...]).sum().to(s.device)
-                r = self.dopamine_per_spike*sum(target_spikes)
+                r = self.dopamine_per_spike*target_spikes
                 expected_r = self.dopamine_per_spike*sum(label_spikes)
                 self.dopamine =  self.dopamine + self.learning_rate*(r-self.gamma*expected_r)
 
