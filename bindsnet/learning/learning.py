86c8c9b12140dcd27bf41ffcff4fc2b1b4398966
hafezgh <ghaemi.hafez@gmail.com>
2021-08-19 07:52:41 +0200

    fix local rewarding

--- bindsnet/learning/learning.py
+++ bindsnet/learning/learning.py
@@ -795,7 +795,7 @@ class MSTDPET(LearningRule):
         self.eligibility_trace += self.eligibility / self.tc_e_trace
 
         # Compute weight update.
-        if self.local_rewarding == True and self.target_name.startswith('output'):
+        if self.local_rewarding == True and self.target_name.startswith('output') and self.pred_label is not None:
             print(self.pred_label, self.neuron_per_class)
             self.pred_label_mask = torch.zeros(*self.connection.w.shape).to(self.connection.w.device)
             self.pred_label_mask[...,self.pred_label*self.neuron_per_class:(self.pred_label+1)*self.neuron_per_class] = 1.0
