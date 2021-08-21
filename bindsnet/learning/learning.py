281cba2075643cda94a97f5526b23b8237401c64
hafezgh <ghaemi.hafez@gmail.com>
2021-08-21 09:44:36 +0200

    test

--- bindsnet/learning/learning.py
+++ bindsnet/learning/learning.py
@@ -807,19 +807,19 @@ class MSTDPET(LearningRule):
         self.eligibility_trace *= torch.exp(-self.connection.dt / self.tc_e_trace)
         self.eligibility_trace += self.eligibility / self.tc_e_trace
         # Compute weight update.
-        # if self.local_rewarding == True and self.target_name.startswith('output') and self.pred_label is not None:
-        #     self.pred_label_mask = torch.zeros(*self.connection.w.shape).to(self.connection.w.device)
-        #     self.pred_label_mask[...,self.pred_label*self.neuron_per_class:(self.pred_label+1)*self.neuron_per_class] = 1.0
-        #     # print(self.pred_label_mask)
-        #     # print(self.connection.w)
-        #     self.connection.w += self.pred_label_mask*(
-        #         self.nu[0] * self.connection.dt * reward * self.eligibility_trace
-        #     )
-        #     #print(self.connection.w)
-        # else:
-        self.connection.w += (
-            self.nu[0] * self.connection.dt * reward * self.eligibility_trace
-        )
+        if self.local_rewarding == True and self.target_name.startswith('output') and self.pred_label is not None:
+            self.pred_label_mask = torch.zeros(*self.connection.w.shape).to(self.connection.w.device)
+            self.pred_label_mask[...,self.pred_label*self.neuron_per_class:(self.pred_label+1)*self.neuron_per_class] = 1.0
+            # print(self.pred_label_mask)
+            # print(self.connection.w)
+            self.connection.w += self.pred_label_mask*(
+                self.nu[0] * self.connection.dt * reward * self.eligibility_trace
+            )
+            #print(self.connection.w)
+        else:
+            self.connection.w += (
+                self.nu[0] * self.connection.dt * reward * self.eligibility_trace
+            )
 
         # Update P^+ and P^- values.
         self.p_plus *= torch.exp(-self.connection.dt / self.tc_plus)
