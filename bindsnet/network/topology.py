d4b4de467031224f90abd8025af0439d6e1e0093
Erfan Mirzaei <erfunmirzaei@gmail.com>
2021-08-22 19:27:08 +0430

    test

--- bindsnet/network/topology.py
+++ bindsnet/network/topology.py
@@ -664,7 +664,7 @@ class LocalConnection(AbstractConnection):
             w = self.w.view(self.source.n, self.target.n)
             print(w,w.shape)
             w *= self.norm / self.w.sum(0).view(1, -1)
-            print(self.norm / self.w.sum(0).view(1, -1),self.norm / self.w.sum(0).view(1, -1).shape)
+            print(self.norm / (self.w.sum(0).view(1, -1)),(self.norm / (self.w.sum(0).view(1, -1))).shape)
     def normalize_meh(self) -> None:
         # language=rst
         """
