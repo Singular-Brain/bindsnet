543bfebf4deaa22ce963d702e4b32e00909fc6ec
Erfan Mirzaei <erfunmirzaei@gmail.com>
2021-08-22 20:04:14 +0430

    test

--- bindsnet/network/topology.py
+++ bindsnet/network/topology.py
@@ -662,7 +662,7 @@ class LocalConnection(AbstractConnection):
         """
         if self.norm is not None:
             w = self.w.view(self.source.n, self.target.n)
-            print(w[0,:],w.shape)
+            print(w[:,0],w[:,0].shape)
             w *= self.norm / self.w.sum(0).view(1, -1)
             print(self.norm / (self.w.sum(0).view(1, -1)),(self.norm / (self.w.sum(0).view(1, -1))).shape)
     def normalize_meh(self) -> None:
