0c9adcc59a8bba84ede9a9f5452928a145f84ca2
Mahbodnr <mahbodnouri@gmail.com>
2021-08-26 12:55:23 +0430

    complete LC topology

--- bindsnet/network/topology.py
+++ bindsnet/network/topology.py
@@ -802,7 +802,6 @@ class LocalConnection(AbstractConnection):
         # s: batch, ch_in, w_in, h_in => s_unfold: batch, ch_in, ch_out * w_out * h_out, k ** 2
         # w: ch_in, ch_out * w_out * h_out, k ** 2
         # a_post: batch, ch_in, ch_out * w_out * h_out, k ** 2 => batch, ch_out * w_out * h_out (= target.n)
-        print(s.shape, self.kernel_size)
         s_unfold = s.unfold(
             -2,self.kernel_size[0],self.stride[0]
         ).unfold(
